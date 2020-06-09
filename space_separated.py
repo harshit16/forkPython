d043c1f958546eb68e704b77df8e6c8f849f9a26
harshit16 <harsh.spn16@gmail.com>
2020-06-09 23:48:39 +0530

    Create space_separated.py

---
+++ space_separated.py
@@ -0,0 +1,25 @@
+"""
+
+"""
+
+#User function Template for python3
+
+# Function to print words with spaces
+def print_fun():
+    # Your code here
+    print('Geeks','For','Geeks',sep=' ')
+    # Do not use space in the string
+    # try to use seperator
+
+
+#{ 
+#Driver Code Starts.
+
+# Python Code to print Words with Space seperated
+def main():
+    print_fun()
+
+if __name__ == '__main__':
+    main()
+
+#} Driver Code Ends
