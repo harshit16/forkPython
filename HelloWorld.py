2c4bf916a6f780459a2a1560acd86bcc1888a0ab
harshit16 <harsh.spn16@gmail.com>
2020-06-09 23:45:16 +0530

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,24 @@
+""" 
+User Task:
+Your task is to complete the function print_fun() to print hello world.
+"""
+
+
+
+
+def print_fun():
+    
+    # Your code here
+    print('Hello World')
+    # Please follow proper indentation
+
+
+#{ 
+#Driver Code Starts.
+
+def main():
+    print_fun()
+
+if __name__ == '__main__':
+    main()
+#} Driver Code Ends
